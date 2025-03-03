67b2bb91c70c66741097d1700739091f45ab06a6
xavisec <xavisec@protonmail.com>
2025-03-03 18:53:24 +0000

    Start of project

---
+++ Code.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import dask.dataframe as ddf
+
+TripsFullData = pd.read_csv('./DataSources/TripsFullData.csv')
+TripsByDistance = pd.read_csv('./DataSources/TripsByDistance.csv')
+TripsByDistance = pd.read_csv('./DataSources/TripsByDistance.csv')
+
