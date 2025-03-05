4d253dc59e9c4052fae3da3ceda6f62dc4be47a3
xavisec <xavisec@protonmail.com>
2025-03-05 22:23:49 +0000

    Start of project

--- Code.py
+++ Code.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import dask.dataframe as dd
 
+
+# Read the CSV files into Pandas and Dask DataFrames
 TripsFullDataPandas = pd.read_csv('./DataSources/TripsFullData.csv')
 TripsByDistancePandas = pd.read_csv('./DataSources/TripsByDistance.csv')
 TripsFullDataDask = dd.read_csv('./DataSources/TripsFullData.csv')
